1508c9eaebfffd0225039c2ef9630a9a859e1a7f
Molemo07 <molemomadumo@gmail.com>
2023-02-16 14:40:07 -0800

    My first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/*
- * main - print aline of code using puts
+/**
+ * main - print a line of code using puts
  *
  * Return : 0
- */
-int main (void) 
-{ 
-	puts ("\"Programming is like building a multilingual puzzle");
+*/
+void  main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
