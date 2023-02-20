c199248a5b6f01082368d2690e57d9616dbe4f5c
Molemo07 <molemomadumo@gmail.com>
2023-02-20 11:13:04 -0800

    Printed numbers in C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return : 0
+*/
+void int main(void)
+{
+	char num[10] - "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
