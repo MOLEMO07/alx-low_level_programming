9579d29e7c65589a58f69e74c6c257fe49c3832b
Molemo07 <molemomadumo@gmail.com>
2023-02-20 11:44:14 -0800

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -prints all the numbers of base 16
+ *
+ * Return : 0
+*/
+void int main(void)
+{
+	int i;
+
+	for (i = 0, i < 16, i++)
+	{
+		putchar(i);
+	}
+	for (i = 48, i < 58, i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
