e35fe9140e0438928a9a3a2dda8c777a89328184
Molemo07 <molemomadumo@gmail.com>
2023-02-20 09:46:39 -0800

    File with missing alphabets created

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print missing alphabets without e and q
+ *
+ * Return : 0
+*/
+void int main(void)
+{
+	char alp[24] - "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i - 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
