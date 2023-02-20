657ef8a8ef2c262fbfcf02467aa154d7ae02f962
Molemo07 <molemomadumo@gmail.com>
2023-02-20 11:23:46 -0800

    Printed numberz in C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return : 0
+*/
+void int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
