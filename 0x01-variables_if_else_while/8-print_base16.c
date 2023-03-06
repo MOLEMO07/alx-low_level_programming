42ab5021a6f768e4f2287cab22607c94f90ae293
Molemo07 <molemomadumo@gmail.com>
2023-03-06 10:54:59 -0800

    C-Variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
- * main -prints all the numbers of base 16
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
- * Return : 0
-*/
-void int main(void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i;
 
-	for (i = 0, i < 16, i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i = 48, i < 58, i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
